53f178e2f5a6178e284465b792d14038a4af6a5c
Tom Joy <thwjoy@fb.com>
2022-03-31 16:32:35 +0000

    changing env name

--- run_sernam.sh
+++ run_sernam.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+conda activate mmf
 cd slurm-runner
 python launch_multiple.py --config dataset1_configs/test
--- upload_sernam.sh
+++ upload_sernam.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+conda activate mmf
 cd slurm-runner
 python upload_logs.py

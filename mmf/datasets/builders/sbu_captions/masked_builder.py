7018a766bcbafe9ee242577059e215445834e5bc
Tom Joy <tom.joy94@gmail.com>
2022-04-12 11:08:26 +0100

    SBU working

--- mmf/datasets/builders/sbu_captions/masked_builder.py
+++ mmf/datasets/builders/sbu_captions/masked_builder.py
@@ -1,18 +1,66 @@
+# # Copyright (c) Facebook, Inc. and its affiliates.
+
+# from mmf.common.registry import registry
+# from mmf.datasets.builders.coco import MaskedCOCOBuilder
+
+# from .masked_dataset import MaskedSBUDataset
+
+
+# @registry.register_builder("masked_sbu")
+# class MaskedSBUBuilder(MaskedCOCOBuilder):
+#     def __init__(self):
+#         super().__init__()
+#         self.dataset_name = "masked_sbu"
+#         self.set_dataset_class(MaskedSBUDataset)
+
+#     @classmethod
+#     def config_path(cls):
+#         return "configs/datasets/sbu_captions/masked.yaml"
+
 # Copyright (c) Facebook, Inc. and its affiliates.
+import logging
+
 
 from mmf.common.registry import registry
-from mmf.datasets.builders.coco import MaskedCOCOBuilder
+from mmf.datasets.mmf_dataset_builder import MMFDatasetBuilder
+from mmf.utils.general import get_mmf_root
 
 from .masked_dataset import MaskedSBUDataset
 
+import os
+
+logger = logging.getLogger(__name__)
+
 
 @registry.register_builder("masked_sbu")
-class MaskedSBUBuilder(MaskedCOCOBuilder):
+class MaskedSBUBuilder(MMFDatasetBuilder):
     def __init__(self):
-        super().__init__()
         self.dataset_name = "masked_sbu"
+        super().__init__(dataset_name=self.dataset_name)
         self.set_dataset_class(MaskedSBUDataset)
+        self.download_url = "https://www.cs.virginia.edu/~vicente/sbucaptions/SBUCaptionedPhotoDataset.tar.gz"
+
+    def build(self, config, dataset):
+        download_folder = os.path.join(
+            config.data_dir, "sbu"
+        )
+
+        file_name = self.download_url.split("/")[-1]
+        local_filename = os.path.join(download_folder, file_name)
+
+        # Either if the zip file is already present or if there are some
+        # files inside the folder we don't continue download process
+        if os.path.exists(local_filename):
+            return
+
+        raise Exception("You need to download %s into the folder %s" % (self.download_url, download_folder))
+
+
+    def load(self, config, dataset, *args, **kwargs):
+        self.dataset = self.dataset_class(config, dataset, 0)
+        return self.dataset
 
     @classmethod
     def config_path(cls):
         return "configs/datasets/sbu_captions/masked.yaml"
+
--- mmf/datasets/builders/sbu_captions/masked_dataset.py
+++ mmf/datasets/builders/sbu_captions/masked_dataset.py
@@ -1,13 +1,71 @@
+# # Copyright (c) Facebook, Inc. and its affiliates.
+
+# from mmf.datasets.builders.coco import MaskedCOCODataset
+
+
+# class MaskedSBUDataset(MaskedCOCODataset):
+#     def __init__(self, config, dataset_type, imdb_file_index, *args, **kwargs):
+#         super().__init__(config, dataset_type, imdb_file_index, *args, **kwargs)
+#         self.dataset_name = "masked_sbu"
+#         self._two_sentence = config.get("two_sentence", True)
+#         self._false_caption = config.get("false_caption", True)
+#         self._two_sentence_probability = config.get("two_sentence_probability", 0.5)
+#         self._false_caption_probability = config.get("false_caption_probability", 0.5)
+
+
 # Copyright (c) Facebook, Inc. and its affiliates.
 
-from mmf.datasets.builders.coco import MaskedCOCODataset
+from mmf.datasets.base_dataset import BaseDataset
+from mmf.common.sample import Sample
 
+from PIL import Image
+import requests
+from io import BytesIO
+import pandas as pd
+import os
 
-class MaskedSBUDataset(MaskedCOCODataset):
-    def __init__(self, config, dataset_type, imdb_file_index, *args, **kwargs):
-        super().__init__(config, dataset_type, imdb_file_index, *args, **kwargs)
-        self.dataset_name = "masked_sbu"
-        self._two_sentence = config.get("two_sentence", True)
-        self._false_caption = config.get("false_caption", True)
-        self._two_sentence_probability = config.get("two_sentence_probability", 0.5)
-        self._false_caption_probability = config.get("false_caption_probability", 0.5)
+class MaskedSBUDataset(BaseDataset):
+    def __init__(self, config, dataset, imdb_file_index, *args, **kwargs):
+        if "name" in kwargs:
+            name = kwargs["name"]
+        elif "dataset_name" in kwargs:
+            name = kwargs["dataset_name"]
+        else:
+            name = "masked_sbu"
+        super().__init__(name, config, dataset, index=imdb_file_index)
+
+        self.photos = []
+        self.captions = []
+        file1 = os.path.join(self.config.data_dir, config.features)
+        file2 = os.path.join(self.config.data_dir, config.annotations)
+
+        for line1, line2 in zip(open(file1), open(file2)):
+            url = line1.rstrip()
+            caption = line2.rstrip()
+            self.photos.append(url)
+            self.captions.append(caption)      
+
+    def init_processors(self):
+        super().init_processors()
+
+    def __getitem__(self, idx):
+        current_sample = Sample()
+        url, caption = self.photos[idx], self.captions[idx]
+        caption_data = {"text": caption}
+        try:
+            response = requests.get(url)
+            img = Image.open(BytesIO(response.content))
+            current_sample.image = self.image_processor(img)
+        except:
+            return None
+        processed_question = self.text_processor(caption_data)
+        current_sample.text = processed_question["text"]
+        if "input_ids" in processed_question:
+            current_sample.update(processed_question)
+
+        return current_sample
+
+    
+    def __len__(self):
+        return len(self.photos)
+
